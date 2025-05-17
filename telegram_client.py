0ba89c9e2b747d75725d7921ae50966be5dcfea9
OlegGorsky <118285892+OlegGorsky@users.noreply.github.com>
2025-05-18 00:05:47 +0300

    Update telegram_client.py

--- telegram_client.py
+++ telegram_client.py
@@ -2,8 +2,7 @@ from pyrogram import Client
 import os
 
 app = Client(
-    "bot_session",
+    "user_session",  # Название сессии → создастся файл user_session.session
     api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN")
+    api_hash=os.getenv("API_HASH")
 )
